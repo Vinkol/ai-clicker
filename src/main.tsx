6ed935d88e2e79ac1fb6163d157c912f1435242a
Dmitriy Yashin <vinkoldima@gmail.com>
2025-03-10 15:49:01 +0300

    add SpeedInsights

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';  
 import './index.css';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
@@ -9,3 +10,5 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+injectSpeedInsights();
